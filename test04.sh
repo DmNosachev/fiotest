3df7fd883f186718cdf4a2745560ae097350f1cc
Dmitry Nosachev <quartz64@gmail.com>
2020-09-01 14:54:14 +0300

    Add files via upload

---
+++ test04.sh
@@ -0,0 +1,77 @@
+# SNIA Solid State Storage (SSS) Performance Test Specification (PTS) implementation.
+# See http://www.snia.org/tech_activities/standards/curr_standards/pts for more info.
+
+#!/bin/bash
+
+# SNIA PTS Test 04: Write Saturation
+
+usage()
+{
+	echo "Usage: $0 /dev/<device to test>"
+    exit 0
+}
+
+readonly OIO=16;
+readonly THREADS=8;
+readonly ROUNDS=600;
+FIO="/usr/bin/fio"
+readonly TEST_NAME="04_Write_Saturation_test"
+LOG_FILE=${TEST_NAME}/results/test.log
+TIMESTAMP=$(date +%Y-%m-%d %H:%M:%S)
+
+if [ $# -lt 1 ] ; then
+	usage
+fi
+
+if [ ! -e $1 ] ; then
+	usage
+fi
+
+hash $FIO 2>/dev/null || { echo >&2 "This script requires fio (http://git.kernel.dk/?p=fio.git) but it's not installed."; exit 1; }
+
+#The output from a test run is placed in the ./results folder.
+#This folder is recreated after every run.
+
+rm -rf ${TEST_NAME}/results > /dev/null
+mkdir -p ${TEST_NAME}/results
+
+# Test and device information
+echo "$TIMESTAMP Running ${TEST_NAME} on device: $1" >> $LOG_FILE
+
+
+echo "Device information:" >> $LOG_FILE
+smartctl -i $1 >> $LOG_FILE
+
+#purge the device
+
+#hdparm --user-master u --security-set-pass PasSWorD $1
+#hdparm --user-master u --security-erase PasSWorD $1
+
+/usr/sbin/nvme format $1
+
+echo "$TIMESTAMP Purge done" >> $LOG_FILE
+
+echo "OIO/thread = $OIO, Threads = $THREADS" >> $LOG_FILE
+$FIO --version >> $LOG_FILE
+echo "Test Start time: `date`" >> $LOG_FILE
+
+#Workload independent preconditioning
+#Run SEQ Workload Independent Preconditioning - Write 2X User Capacity with 128KiB SEQ writes, writing to the entire ActiveRange without LBA restrictions
+
+$FIO --name=precondition --filename=$1 --iodepth=16 --numjobs=1 --bs=128k --ioengine=libaio --rw=write --group_reporting --direct=1 --thread --refill_buffers --loops=2
+echo "$TIMESTAMP Preconditioning done" >> $LOG_FILE
+
+echo "$TIMESTAMP Starting test $TEST_NAME" >> $LOG_FILE
+
+#Test 
+echo "Starting test $TEST_NAME"
+
+for PASS in $(eval echo {1..$ROUNDS});
+do
+	$FIO --output-format=json --name=job --filename=$1 --iodepth=$OIO --numjobs=$THREADS --bs=4096 --ioengine=libaio --rw=randwrite --group_reporting --runtime=60 --time_based --direct=1 --randrepeat=0 --norandommap --thread --refill_buffers --output=${TEST_NAME}/results/fio_pass=${PASS}.json
+	clear
+    echo -e "$TIMESTAMP ${TEST_NAME} pass $PASS of $ROUNDS done" >> $LOG_FILE
+done
+
+exit 0
+
